d096d09a0101acdec5f178d565f572b5b17a6711
mohil194250 <52916471+mohil194250@users.noreply.github.com>
2022-12-23 17:01:35 +0530

    Update namespacecheck.sh

--- namespacecheck.sh
+++ namespacecheck.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
  namespace = $(kubectl get ns $1 -o json | jq .status.phase -r) 
- if [ $namespace == "Active" ] {
+ if [ $namespace == "Active" ] 
       echo "Namespace exists"
  } else {
       kubectl create namespace $1
